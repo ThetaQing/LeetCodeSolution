c4c3badfd7e332af48ee4a15c4841b96f826f932
ThetaQing <csusq2016@163.com>
2019-11-12 20:59:02 +0800

    初级算法（数组）——删除重复元素更优解
    双指针法，LeetCode执行时间24ms， 92.30%

--- LeetCodeSolution/LeetCodeSolution/basic_algorithm_array.cpp
+++ LeetCodeSolution/LeetCodeSolution/basic_algorithm_array.cpp
@@ -13,32 +13,31 @@ using std::vector;
 * 函数名：int removeDuplicates(vector<int>& nums)
 * 函数参数： 一个有序数组，数组中的数可能有重复
 * 函数返回值：去除重复元素之后的数组长度
-* 函数算法： 用第i数与第i-1个数进行比较，相等则删除前一个数
-			 此时指针不需要移动，因为数组长度减小，索引也向后移动了一个位置
-			 即此时的nums[i]是数组未变化前的nums[i+1]。
-			 如果不相等，移动指针到下一个位置，直到最后。
-* 算法分析：执行速度慢，LeetCode运行时间260ms，12.32%			
-
+* 函数算法： 双指针法：
+			1、放置两个指针 i 和 j，其中 i 是慢指针，而 j 是快指针。
+			2、只要 nums[i] = nums[j]，我们就增加 j 以跳过重复项。
+			3、当我们遇到 nums[j] ！= nums[i]时，跳过重复项的运行已经结束，因此我们必须把它（nums[j]）的值复制到 nums[i + 1]。
+			4、递增 i，接着我们将再次重复相同的过程，直到 j 到达数组的末尾为止。
+* 算法分析：时间复杂度：O(n)  24ms ，92.3%
+			空间复杂度：O(1)  9.9m，81.34%
+* 易错点：当输入为空的时候要单独考虑返回0，否则会返回1.
+* 个人总结：和前一个算法最大的不同在于，该算法一次性解决同一个重复的数，并且是将新的数直接覆盖重复的数，减少了移动元素的时间和多次重复项的处理时间。
 **/
 int CArray::removeDuplicates(vector<int>& nums)
 {
-	int len = nums.size();
-
-	for (int i = 1; i < len; )
+	if (nums.empty())
+		return 0;
+	int i = 0;
+	for ( int j = 1; j < nums.size();++j)
 	{
-		if (i >= 1 && nums[i] == nums[i - 1])
-		{
-			nums.erase(nums.begin() + i - 1);
-			len = nums.size();
-			// i不需要发生变化就已经移动了，因为长度发生了变化，索引随之变化
-		}
-		else
+		if (nums[j] != nums[i])  // 直到出现没有重复的元素
 		{
 			i += 1;
-
-		}
-
+			nums[i] = nums[j];
+		}	
+		
 	}
-	return len;
+	nums.erase(nums.begin() + i + 1, nums.end());  // 如果要求原数组相应发生变化的话
+	return i + 1;
 
 }
